78c79a825f1ce8853232d1eb7f0c23838a9f6f5e
Natan Martins <natan.n.martins20@gmail.com>
2021-06-17 13:06:38 -0300

    Criado novas classes para o projeto

---
+++ Models/TipoPano.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoPanosDePratoETapetes.Models
+{
+   public class TipoPano
+    {
+        int _id;
+        string _nome;
+        double _precoUnidadeFabricacao;
+
+        public TipoPano(int id, string nome, double precoUnidadeFabricacao)
+        {
+            _id = id;
+            _nome = nome;
+            _precoUnidadeFabricacao = precoUnidadeFabricacao;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Nome { get => _nome; set => _nome = value; }
+        public double PrecoUnidadeFabricacao { get => _precoUnidadeFabricacao; set => _precoUnidadeFabricacao = value; }
+    }
+}
